56f2aa0cb8277af1830b0e7554e280f526d78686
Karishma Mandyam <karishma.mandyam@gmail.com>
2018-11-27 16:36:59 -0800

    adds function call structure to keep.py

--- keep.py
+++ keep.py
@@ -1,15 +1,29 @@
 import gkeepapi
 import sys
 
-with open(sys.argv[1], "r") as auth_file:
-    username = auth_file.readline()
-    password = auth_file.readline()
+def create_note(keep):
+    """
+    Usage Example
 
-keep = gkeepapi.Keep()
-success = keep.login(username, password)
+    note = keep.createNote('New Note', 'Write Code')
+    note.pinned = True
+    note.color = gkeepapi.node.ColorValue.Red
+    keep.sync()
+    """
+    note = keep.createNote('Test Note', 'Testing Scheduler')
+    note.color = gkeepapi.node.ColorValue.Purple
+    keep.sync()
 
-note = keep.createNote('HAHA', 'Eat breakfast')
-note.pinned = True
-note.color = gkeepapi.node.ColorValue.Red
-note.checked = True
-keep.sync()
+
+def main():
+    with open(sys.argv[1], "r") as auth_file:
+        username = auth_file.readline()
+        password = auth_file.readline()
+
+    keep = gkeepapi.Keep()
+    success = keep.login(username, password)
+
+    create_note(keep)
+
+if __name__ == '__main__':
+    main()
